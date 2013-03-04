d02a35d93f51c725f29b4632daf3be599f7a5ff7
Yuri Cauwerts <yuri.cauwerts@groept.be>
2013-03-04 12:33:09 +0100

    Hydra branch

--- Main.java
+++ Main.java
@@ -4,14 +4,18 @@ public class Main {
 	public static void main(String args[]){
 		Morse m = new Morse();
 		m.out("Test");
+		Hydra h = new Hydra();
+
 	}
 }
 
 final class Morse {
 	public Morse(){
-		
 	}
 	public void out(String s){
 		System.out.println(s);
 	}
+}
+
+final class Hydra{
 }
